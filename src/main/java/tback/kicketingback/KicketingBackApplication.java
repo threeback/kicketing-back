3a86f3ec865b70a05bec203c6a7c1482e26cb8a3
CodingApe9 <pos06098@gmail.com>
2024-03-11 23:39:19 +0900

    chore: init project

---
+++ src/main/java/tback/kicketingback/KicketingBackApplication.java
@@ -0,0 +1,13 @@
+package tback.kicketingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KicketingBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KicketingBackApplication.class, args);
+	}
+
+}
