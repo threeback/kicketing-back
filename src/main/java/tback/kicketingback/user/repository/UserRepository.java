8a9cc318ae13f258df30b6a4bba6c7abff4e3e83
cottonrose1011 <pjeka7er43@gmail.com>
2024-04-02 16:55:05 +0900

    feat : add existByEmail

--- src/main/java/tback/kicketingback/user/repository/UserRepository.java
+++ src/main/java/tback/kicketingback/user/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package tback.kicketingback.user.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.repository.Repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import tback.kicketingback.user.domain.User;
 
-public interface UserRepository extends Repository<User, Long> {
-
-	Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
