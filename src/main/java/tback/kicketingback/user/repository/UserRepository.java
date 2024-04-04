d08d0e344d2ff686c6ead0272e72a824a2eb3bcb
cottonrose1011 <pjeka7er43@gmail.com>
2024-04-04 12:26:36 +0900

    feat : 회원가입에 필요한 메소드 추가

--- src/main/java/tback/kicketingback/user/repository/UserRepository.java
+++ src/main/java/tback/kicketingback/user/repository/UserRepository.java
@@ -1,8 +1,16 @@
 package tback.kicketingback.user.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+import org.springframework.data.repository.Repository;
+
 import tback.kicketingback.user.domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
+public interface UserRepository extends Repository<User, Long> {
+
+	Optional<User> findByEmail(String email);
+
+	void save(User user);
+
+	boolean existsByEmail(String email);
 }
