34a413a40b55715a0ad596b4d5683d72e1e50899
cottonrose1011 <pjeka7er43@gmail.com>
2024-04-04 12:29:20 +0900

    feat : 비밀번호 암호화

---
+++ src/test/java/tback/kicketingback/global/encode/PasswordEncoderSHA256Test.java
@@ -0,0 +1,23 @@
+package tback.kicketingback.global.encode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PasswordEncoderSHA256Test {
+
+	@Test
+	@DisplayName("SHA-256 암호화 제대로 되었는지 확인함.")
+	public void 인코딩_테스트() {
+		// Given
+
+		final String rawPassword = "123413f";
+		String encoded = "";
+
+		// When
+		encoded = PasswordEncoderSHA256.encode(rawPassword);
+
+		// Then
+		Assertions.assertThat(encoded).isEqualTo(PasswordEncoderSHA256.encode(rawPassword));
+	}
+}
