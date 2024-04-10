f71f98f236e9e45a26b305372883895cb58437e5
cottonrose1011 <pjeka7er43@gmail.com>
2024-04-10 18:57:54 +0900

    refact : 예외 메세지 컨벤션 일치 리팩토링

--- src/main/java/tback/kicketingback/user/exception/exceptions/EmailSendException.java
+++ src/main/java/tback/kicketingback/user/exception/exceptions/EmailSendException.java
@@ -2,6 +2,6 @@ package tback.kicketingback.user.exception.exceptions;
 
 public class EmailSendException extends RuntimeException {
 	public EmailSendException() {
-		super("회원가입 이메일 검증 코드 전송 실패");
+		super("회원가입 이메일 검증 코드 전송에 실패했습니다.");
 	}
 }
