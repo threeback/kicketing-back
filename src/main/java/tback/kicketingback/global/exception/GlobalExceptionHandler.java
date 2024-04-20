1d87f01a972b259a8552586d8825d3143c4abd88
pwrwpw <dlgustjr9912@gmail.com>
2024-04-20 21:35:38 +0900

    feat : 검증 어노테이션 추가에 따른 예외 처리

---
+++ src/main/java/tback/kicketingback/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package tback.kicketingback.global.exception;
+
+import java.util.Objects;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends AbstractExceptionHandler{
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<String> MethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		return getBadRequestResponseEntity(Objects.requireNonNull(exception.getFieldError()).getDefaultMessage());
+	}
+}
