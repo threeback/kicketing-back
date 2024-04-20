b829cfd0700dacd2503f6d86c7ac341c3abe1fcd
pwrwpw <dlgustjr9912@gmail.com>
2024-04-20 21:34:19 +0900

    feat : 로깅 출력시 예외 클래스 출력

--- src/main/java/tback/kicketingback/global/exception/AbstractExceptionHandler.java
+++ src/main/java/tback/kicketingback/global/exception/AbstractExceptionHandler.java
@@ -8,19 +8,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class AbstractExceptionHandler {
 	protected final ResponseEntity<String> getNotFoundResponseEntity(final String message) {
-		log.info("response send because of NOT_FOUND: " + message);
+		log.info(this.getClass() + "response send because of NOT_FOUND: " + message);
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 			.body(message);
 	}
 
 	protected final ResponseEntity<String> getBadRequestResponseEntity(final String message) {
-		log.info("response send because of Bad Request: " + message);
+		log.info(this.getClass() + "response send because of Bad Request: " + message);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 			.body(message);
 	}
 
 	protected final ResponseEntity<String> getForbiddenResponseEntity(final String message) {
-		log.info("response send because of Forbidden: " + message);
+		log.info(this.getClass() + "response send because of Forbidden: " + message);
 		return ResponseEntity.status(HttpStatus.FORBIDDEN)
 			.body(message);
 	}
