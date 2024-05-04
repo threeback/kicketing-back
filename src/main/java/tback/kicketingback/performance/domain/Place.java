b5241a44cc4c4bec88e7fee8e55eefcce0d4ea0c
pwrwpw <dlgustjr9912@gmail.com>
2024-05-04 14:47:56 +0900

    feat : 공연 예매 도메인 엔티티 작성

---
+++ src/main/java/tback/kicketingback/performance/domain/Place.java
@@ -0,0 +1,27 @@
+package tback.kicketingback.performance.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Place {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String address;
+
+	@Column(nullable = false)
+	private String hall;
+
+	protected Place() {
+	}
+}
