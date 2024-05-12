cecd5a7d6b646afd515d24ffe0716f32bb279349
pwrwpw <dlgustjr9912@gmail.com>
2024-05-12 20:30:09 +0900

    feat : 배우 엔티티 생성

---
+++ src/main/java/tback/kicketingback/performance/domain/Star.java
@@ -0,0 +1,33 @@
+package tback.kicketingback.performance.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import tback.kicketingback.performance.domain.type.Sex;
+
+@Entity
+public class Star {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String name;
+
+	@Column(nullable = false)
+	private LocalDate birthdate;
+
+	@Column(nullable = false, length = 6)
+	@Enumerated(EnumType.STRING)
+	private Sex sex;
+
+	@Column
+	private String imageURL;
+}
