783dac99db60ad928ed0055cbfe128b949707820
pwrwpw <dlgustjr9912@gmail.com>
2024-05-19 02:01:35 +0900

    feat : OnStageService 기능 추가

---
+++ src/test/java/tback/kicketingback/performance/service/OnStageServiceTest.java
@@ -0,0 +1,36 @@
+package tback.kicketingback.performance.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tback.kicketingback.performance.dto.GetBookableDatesRequest;
+import tback.kicketingback.performance.dto.SimpleOnStageDTO;
+
+@SpringBootTest
+public class OnStageServiceTest {
+
+	@Autowired
+	private OnStageService onStageService;
+
+	@Test
+	@DisplayName("[정상] UUID로 예매 가능한 공연 날짜 제공")
+	void getBookableDates() {
+		UUID performanceUUID = UUID.fromString("069939e9-dfed-46b5-832e-996cd737584a");
+		LocalDate startDate = LocalDate.of(2024, 5, 1);
+		LocalDate endDate = LocalDate.of(2024, 5, 31);
+		List<SimpleOnStageDTO> simpleOnStageDTOS = onStageService.getBookableDates(performanceUUID, new GetBookableDatesRequest(startDate));
+
+		for (SimpleOnStageDTO dto : simpleOnStageDTOS) {
+			assertThat(dto.dateTime()).isAfterOrEqualTo(startDate.atStartOfDay());
+			assertThat(dto.dateTime()).isBeforeOrEqualTo(endDate.atStartOfDay());
+		}
+	}
+}
--- src/test/java/tback/kicketingback/performance/service/PerformanceServiceTest.java
+++ src/test/java/tback/kicketingback/performance/service/PerformanceServiceTest.java
@@ -3,11 +3,8 @@ package tback.kicketingback.performance.service;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,9 +12,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import tback.kicketingback.performance.dto.GetBookableDatesRequest;
 import tback.kicketingback.performance.dto.GetPerformancesRequest;
-import tback.kicketingback.performance.dto.SimpleOnStageDTO;
 import tback.kicketingback.performance.dto.SimplePerformancePlaceDTO;
 
 @SpringBootTest
@@ -43,20 +38,6 @@ class PerformanceServiceTest {
 			new GetPerformancesRequest("day", 10), LocalDate.now());
 
 		assertThat(simplePerformancePlaceDTOS.size()).isGreaterThanOrEqualTo(0);
-	}
-
-	@Test
-	@DisplayName("[정상] UUID로 예매 가능한 공연 날짜 제공")
-	void getBookableDates() {
-		UUID performanceUUID = UUID.fromString("069939e9-dfed-46b5-832e-996cd737584a");
-		LocalDate startDate = LocalDate.of(2024, 5, 1);
-		LocalDate endDate = LocalDate.of(2024, 5, 31);
-		List<SimpleOnStageDTO> simpleOnStageDTOS = performanceService.getBookableDates(performanceUUID, new GetBookableDatesRequest(startDate));
-
-		for (SimpleOnStageDTO dto : simpleOnStageDTOS) {
-			assertThat(dto.dateTime()).isAfterOrEqualTo(startDate.atStartOfDay());
-			assertThat(dto.dateTime()).isBeforeOrEqualTo(endDate.atStartOfDay());
-		}
 	}
 }
 
