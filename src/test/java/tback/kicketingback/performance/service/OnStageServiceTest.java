4814f6df379ae28b29e18297be8a5b026f10c03d
pwrwpw <dlgustjr9912@gmail.com>
2024-05-19 03:44:14 +0900

    test: 존재하지 않는 공연일 경우 예외 발생 테스트 코드 작성

--- src/test/java/tback/kicketingback/performance/service/OnStageServiceTest.java
+++ src/test/java/tback/kicketingback/performance/service/OnStageServiceTest.java
@@ -13,12 +13,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import tback.kicketingback.performance.dto.GetBookableDatesRequest;
 import tback.kicketingback.performance.dto.SimpleOnStageDTO;
+import tback.kicketingback.performance.exception.exceptions.InvalidPerformanceUUIDException;
 
 @SpringBootTest
 public class OnStageServiceTest {
 
 	@Autowired
 	private OnStageService onStageService;
+
+	@Test
+	@DisplayName("[실패] 존재하지 않는 공연 ID로 조회 시 예외 발생")
+	void getUnorderedReservationsCountByGrade() {
+		LocalDate startDate = LocalDate.of(2024, 5, 1);
+		assertThatThrownBy(() -> onStageService.getBookableDates(UUID.randomUUID(),new GetBookableDatesRequest(startDate)))
+			.isInstanceOf(InvalidPerformanceUUIDException.class);
+	}
 
 	@Test
 	@DisplayName("[정상] UUID로 예매 가능한 공연 날짜 제공")
