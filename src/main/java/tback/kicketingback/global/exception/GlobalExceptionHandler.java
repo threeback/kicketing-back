d361a58c2d00919c5e59c277561006660f3b7357
CodingApe9 <pos06098@gmail.com>
2024-05-18 16:52:48 +0900

    refact : 사용하지 않는 예외 핸들러 삭제 및 새로운 예외 핸들러 추가
    - @Size가 던지는 예외 ConstraintViolationException 삭제
    - MethodArgumentTypeMismatchException 핸들러 작성

--- src/main/java/tback/kicketingback/global/exception/GlobalExceptionHandler.java
+++ src/main/java/tback/kicketingback/global/exception/GlobalExceptionHandler.java
@@ -6,20 +6,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import jakarta.validation.ConstraintViolationException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends AbstractExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<String> MethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+	public ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
 		return getBadRequestResponseEntity(exception,
 			Objects.requireNonNull(exception.getFieldError()).getDefaultMessage());
 	}
 
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<String> ConstraintViolationException(ConstraintViolationException exception) {
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<String> methodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
 		return getBadRequestResponseEntity(exception, exception.getMessage());
 	}
 }
