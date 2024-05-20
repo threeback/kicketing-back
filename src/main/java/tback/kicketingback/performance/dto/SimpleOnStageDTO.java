dfcd2a1b3aa7822c8e0283b9d13b6dc2c55d986a
pwrwpw <dlgustjr9912@gmail.com>
2024-05-20 11:20:31 +0900

    refactor : dto에서 from을 통해 생성하도록 수정

--- src/main/java/tback/kicketingback/performance/dto/SimpleOnStageDTO.java
+++ src/main/java/tback/kicketingback/performance/dto/SimpleOnStageDTO.java
@@ -2,9 +2,18 @@ package tback.kicketingback.performance.dto;
 
 import java.time.LocalDateTime;
 
+import tback.kicketingback.performance.domain.OnStage;
+
 public record SimpleOnStageDTO(
 	Long id,
 	LocalDateTime dateTime,
 	int round
 ) {
+	public static SimpleOnStageDTO from(OnStage onStage) {
+		return new SimpleOnStageDTO(
+			onStage.getId(),
+			onStage.getDateTime(),
+			onStage.getRound()
+		);
+	}
 }
