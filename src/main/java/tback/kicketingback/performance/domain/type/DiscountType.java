9c4ead199b0120546df60a0ad8797140e00084a5
cottonrose1011 <pjeka7er43@gmail.com>
2024-05-21 22:50:59 +0900

    feat : 할인 대상 검증하는 기능

--- src/main/java/tback/kicketingback/performance/domain/type/DiscountType.java
+++ src/main/java/tback/kicketingback/performance/domain/type/DiscountType.java
@@ -1,5 +1,19 @@
 package tback.kicketingback.performance.domain.type;
 
+import tback.kicketingback.performance.exception.exceptions.InvalidGetDiscountTypeException;
+
 public enum DiscountType {
-	NONE, STUDENT, OLD, PRONUNCIATION
+	NONE, STUDENT, OLD, PRONUNCIATION;
+
+	public static DiscountType of(String discountTypeString) {
+		DiscountType discountType;
+
+		try {
+			discountType = DiscountType.valueOf(discountTypeString.toUpperCase());
+		} catch (IllegalArgumentException exception) {
+			throw new InvalidGetDiscountTypeException(discountTypeString);
+		}
+
+		return discountType;
+	}
 }
