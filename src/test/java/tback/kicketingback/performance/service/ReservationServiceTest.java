70e4a84cdd8c6f85b2d0dc15b308978af1680324
cottonrose1011 <pjeka7er43@gmail.com>
2024-05-21 22:53:20 +0900

    test : 공연예메 완료 테스트

--- src/test/java/tback/kicketingback/performance/service/ReservationServiceTest.java
+++ src/test/java/tback/kicketingback/performance/service/ReservationServiceTest.java
@@ -1,17 +1,21 @@
 package tback.kicketingback.performance.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import tback.kicketingback.performance.dto.SeatGradeCount;
-import tback.kicketingback.performance.exception.exceptions.InvalidOnStageIDException;
+
+import tback.kicketingback.performance.domain.type.DiscountType;
 import tback.kicketingback.performance.dto.GetSeatInfoResponse;
+import tback.kicketingback.performance.dto.SeatGradeCount;
 import tback.kicketingback.performance.exception.exceptions.AlreadySelectedSeatException;
+import tback.kicketingback.performance.exception.exceptions.InvalidOnStageIDException;
 import tback.kicketingback.performance.exception.exceptions.InvalidPerformanceException;
 import tback.kicketingback.performance.exception.exceptions.InvalidSeatIdException;
 import tback.kicketingback.performance.exception.exceptions.NoAvailableSeatsException;
@@ -23,12 +27,11 @@ class ReservationServiceTest {
 
 	@Autowired
 	private ReservationService reservationService;
-  
-  @Autowired
+
+	@Autowired
 	private UserRepository userRepository;
 
-
-  @Test
+	@Test
 	@DisplayName("[예외] 존재 하지 않는 공연을 조회할 경우")
 	void getUnorderedReservationsCountByGrade() {
 		assertThatThrownBy(() -> reservationService.getUnorderedReservationsCountByGrade(0L))
@@ -42,7 +45,8 @@ class ReservationServiceTest {
 			1L);
 
 		assertThat(unorderedReservationsCountByGrade.size()).isGreaterThanOrEqualTo(0);
-  }
+	}
+
 	@Test
 	@DisplayName("[실패] 공연 uuid와 OnStage Id가 연관이 없을 때")
 	void getSeatInfoByMismatchPerformanceUUIDAndOnStageId() {
@@ -119,4 +123,82 @@ class ReservationServiceTest {
 			reservationService.getSeatInfo(performanceUUID, onStageId);
 		}).isInstanceOf(NoAvailableSeatsException.class);
 	}
+
+	// @Test
+	// @DisplayName("[성공] 좌석 예매가 완료됨")
+	// void completeReservation() {
+	// 	Long onStageId = 1L;
+	// 	List<Long> selectedSeatIds = List.of(1L, 2L, 3L);
+	// 	String orderNumber = "testOrderNumber";
+	// 	String testDiscountType = "NONE";
+	// 	User testUser = userRepository.findByEmail("test@test.com").get();
+	// 	DiscountType discountType = DiscountType.valueOf(testDiscountType);
+	//
+	// 	assertThatCode(() -> {
+	// 		reservationService.lockSeats(onStageId, selectedSeatIds, testUser);
+	// 		reservationService.completeReservation(onStageId, orderNumber, discountType, selectedSeatIds, testUser);
+	// 	}).doesNotThrowAnyException();
+	//
+	// }
+
+	@Test
+	@DisplayName("[실패] 좌석 Lock 시간이 만료됨")
+	void expiredLocking() {
+		Long onStageId = 1L;
+		List<Long> selectedSeatIds = List.of(4L, 5L);
+		String orderNumber = "testOrderNumber";
+		String testDiscountType = "NONE";
+		User testUser = userRepository.findByEmail("test@test.com").get();
+		DiscountType discountType = DiscountType.valueOf(testDiscountType);
+
+		assertThrows(AlreadySelectedSeatException.class,
+			() -> reservationService.completeReservation(onStageId, orderNumber, discountType, selectedSeatIds,
+				testUser));
+	}
+
+	@Test
+	@DisplayName("[실패] 좌석 예매는 안했지만 다른 유저가 Lock 을 걸음")
+	void lockedSeat() {
+		Long onStageId = 1L;
+		List<Long> selectedSeatIds = List.of(4L, 5L);
+		String orderNumber = "testOrderNumber";
+		String testDiscountType = "NONE";
+		User testUser = userRepository.findByEmail("pjeka7er43@naver.com").get();
+		DiscountType discountType = DiscountType.valueOf(testDiscountType);
+
+		assertThrows(AlreadySelectedSeatException.class,
+			() -> reservationService.completeReservation(onStageId, orderNumber, discountType, selectedSeatIds,
+				testUser));
+	}
+
+	@Test
+	@DisplayName("[실패] 다른 고객이 예매한 좌석")
+	void alreadyReservation() {
+		Long onStageId = 1L;
+		List<Long> selectedSeatIds = List.of(1L, 2L, 3L);
+		String orderNumber = "testOrderNumber";
+		String testDiscountType = "NONE";
+		User testUser = userRepository.findByEmail("pjeka7er43@naver.com").get();
+		DiscountType discountType = DiscountType.valueOf(testDiscountType);
+
+		assertThrows(AlreadySelectedSeatException.class,
+			() -> reservationService.completeReservation(onStageId, orderNumber, discountType, selectedSeatIds,
+				testUser));
+	}
+
+	@Test
+	@DisplayName("[실패] 존재하지 않는 좌석 예매")
+	void doesntExistSeats() {
+		Long onStageId = 1L;
+		List<Long> selectedSeatIds = List.of(19999L, 299999L, 399999L);
+		String orderNumber = "testOrderNumber";
+		String testDiscountType = "NONE";
+		User testUser = userRepository.findByEmail("test@test.com").get();
+		DiscountType discountType = DiscountType.valueOf(testDiscountType);
+
+		assertThrows(InvalidSeatIdException.class,
+			() -> reservationService.completeReservation(onStageId, orderNumber, discountType, selectedSeatIds,
+				testUser));
+	}
+
 }
