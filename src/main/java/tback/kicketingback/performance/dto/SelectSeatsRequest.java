d52f670f1d006ef4ddef5d370bc1f196279fba4d
cottonrose1011 <pjeka7er43@gmail.com>
2024-05-21 22:51:46 +0900

    refact : LockOnStageSeatsRequest -> SelectSeatsRequest 로 리팩토링

--- src/main/java/tback/kicketingback/performance/dto/SelectSeatsRequest.java
+++ src/main/java/tback/kicketingback/performance/dto/SelectSeatsRequest.java
@@ -6,9 +6,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import tback.kicketingback.performance.exception.exceptions.DuplicateSeatSelectionException;
 
-public record LockOnStageSeatsRequest(List<Long> seatIds) {
+public record SelectSeatsRequest(List<Long> seatIds) {
 
-	public LockOnStageSeatsRequest(
+	public SelectSeatsRequest(
 		@NotNull(message = "좌석을 선택하지 않음")
 		@Size(min = 1, max = 10, message = "좌석 범위 넘음 [1 ~10개 가능]")
 		List<Long> seatIds
