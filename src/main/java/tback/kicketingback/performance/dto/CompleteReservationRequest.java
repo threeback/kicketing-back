4a3ae11e415c862b050794aa7e9e2b72c816656c
pwrwpw <dlgustjr9912@gmail.com>
2024-05-22 17:07:17 +0900

    feat : 입력시 Null 검증하도록 추가

--- src/main/java/tback/kicketingback/performance/dto/CompleteReservationRequest.java
+++ src/main/java/tback/kicketingback/performance/dto/CompleteReservationRequest.java
@@ -1,11 +1,14 @@
 package tback.kicketingback.performance.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CompleteReservationRequest(
+	@NotNull
 	String discountType,
 	@Positive
 	int price,
+	@NotNull
 	SelectSeatsRequest seatsRequest
 ) {
 }
