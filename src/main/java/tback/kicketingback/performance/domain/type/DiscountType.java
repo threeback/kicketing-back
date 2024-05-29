915318aa290439643474952fb5585be30c28382d
cottonrose1011 <pjeka7er43@gmail.com>
2024-05-29 16:08:06 +0900

    feat : 할인 정책 작성

--- src/main/java/tback/kicketingback/performance/domain/type/DiscountType.java
+++ src/main/java/tback/kicketingback/performance/domain/type/DiscountType.java
@@ -1,9 +1,20 @@
 package tback.kicketingback.performance.domain.type;
 
+import java.util.function.Function;
+
 import tback.kicketingback.performance.exception.exceptions.InvalidGetDiscountTypeException;
 
 public enum DiscountType {
-	NONE, STUDENT, OLD, PRONUNCIATION;
+	NONE(price -> 0),
+	STUDENT(price -> (int)(price * 0.1)),
+	OLD(price -> (int)(price * 0.3)),
+	PRONUNCIATION(price -> (int)(price * 0.5));
+
+	private final Function<Integer, Integer> getDiscountAmountFunction;
+
+	DiscountType(Function<Integer, Integer> getDiscountAmountFunction) {
+		this.getDiscountAmountFunction = getDiscountAmountFunction;
+	}
 
 	public static DiscountType of(String discountTypeString) {
 		DiscountType discountType;
@@ -15,5 +26,9 @@ public enum DiscountType {
 		}
 
 		return discountType;
+	}
+
+	public int getDiscountAmount(int price) {
+		return this.getDiscountAmountFunction.apply(price);
 	}
 }
