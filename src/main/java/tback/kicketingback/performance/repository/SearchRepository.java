d0c4904c521f391aae10de0a32633ae8c3c40d33
pwrwpw <dlgustjr9912@gmail.com>
2024-05-31 13:00:35 +0900

    fix : endDate가 지난 공연은 검색이 되지 않도록 수정

--- src/main/java/tback/kicketingback/performance/repository/SearchRepository.java
+++ src/main/java/tback/kicketingback/performance/repository/SearchRepository.java
@@ -1,16 +1,19 @@
 package tback.kicketingback.performance.repository;
 
 import java.util.List;
+
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
+
 import tback.kicketingback.performance.domain.SearchPerformance;
 
 @Repository
 public interface SearchRepository extends ElasticsearchRepository<SearchPerformance, String> {
 
+	@Query("{\"bool\": {\"must\": [{\"match\": {\"name\": {\"query\": \"?0\"}}}], \"filter\": [{\"range\": {\"endDate\": {\"gte\": \"now\"}}}]}}")
 	List<SearchPerformance> findByName(String name);
 
-	@Query("{\"match_phrase_prefix\": {\"name\": {\"query\": \"?0\"}}}")
+	@Query("{\"bool\": {\"must\": [{\"match_phrase_prefix\": {\"name\": {\"query\": \"?0\"}}}], \"filter\": [{\"range\": {\"endDate\": {\"gte\": \"now\"}}}]}}")
 	List<SearchPerformance> findByNameWithAutocomplete(String name);
 }
